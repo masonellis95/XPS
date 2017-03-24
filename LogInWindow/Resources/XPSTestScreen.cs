ee7c11d14554aef8f59a7be152c318a361c50efb
raul escatel <raulaescatel>
2017-03-24 14:51:19 -0500

    Commit Staged

---
+++ LogInWindow/Resources/XPSTestScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogInWindow.Resources
+{
+    public partial class XPSTestScreen : UserControl
+    {
+        public XPSTestScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+          
+            ExamForm exam = new ExamForm();
+            exam.ShowDialog();
+        }
+    }
+}
---
+++ LogInWindow/Results.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogInWindow
+{
+    public partial class Results : Form
+    {
+        
+            
+
+        public Results()
+        {
+            InitializeComponent();
+
+            //correctLabel.Parent = pictureBox1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainMenuForm main = new MainMenuForm();
+            main.ShowDialog();
+
+            
+
+        }
+    }
+}
